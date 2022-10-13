6cb5a951fe3b7f0211c06b7703ddf56745ebbd18
Duong Minh Thong <duongminhthongvn@gmail.com>
2022-10-13 16:20:43 +0700

    update

--- nodejs/crawl/server.js
+++
@@ -1,8 +0,0 @@
-const cheerio = require("cheerio"); // khai báo module cheerio
-
-const request = require("request-promise"); // khai báo module request-promise
-
-request("https://123job.vn/tuyen-dung", (error, response, html) => {
-  // gửi request đến trang
-  console.log(response.statusCode); // 200, kiểm tra xem kết nối thành công không :D
-});
